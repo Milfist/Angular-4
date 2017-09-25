5139272aaf3f0d53c4a937db073a73bbcae138b2
Milfist <milfist@gmail.com>
2017-09-25 10:50:38 +0200

    Test
    Test of commit

--- proyectoCurso/src/app/shopping-list/shopping-list.component.ts
+++ proyectoCurso/src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
+import { ShoppingListService } from './shopping-list.service';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
@@ -8,14 +10,12 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
 
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
-  ];
+  ingredients: Ingredient[];
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this.ingredients = this.slService.getIngredients()
   }
 
   onIngredientAdded(ingredient: Ingredient) {
